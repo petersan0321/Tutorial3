4c6a5ee4a0f07d7d0d9bd6df86e8697db9b23706
Hong Chun San <hongchunsan@Hongs-MacBook-Air-2.local>
2017-02-03 11:29:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hongchunsan/tutorial3/T3_MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hongchunsan.tutorial3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class T3_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_t3_main);
+    }
+}
